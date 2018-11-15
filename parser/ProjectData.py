865f2ee7e26ad16a3d252ce0044cf947a5fda09e
nicolas <nulysse4@gmail.com>
2018-11-14 21:25:10 -0500

    now detect project activity and find closest date

--- parser/ProjectData.py
+++ parser/ProjectData.py
@@ -6,10 +6,12 @@ import logging
 
 class CustomerProject:
 
-    def __init__(self, project_name):
+    def __init__(self, project_name, inactive_delay=pd.Timedelta(3,unit='M'), low_cpi = 90):
         self.parser = InvoiceParser.Invoice()
         self.data = pd.DataFrame()
         self.project_name = project_name
+        self.inactive_delay = inactive_delay
+        self.low_cpi = low_cpi
 
     def load_data(self, invoice_path):
 
@@ -30,10 +32,34 @@ class CustomerProject:
         self.data.set_index('date', inplace=True)
         self.data.index = self.data.index.map(pd.DatetimeIndex.to_pydatetime)
         self.data.sort_index(inplace=True)
-        print(self.data)
+        #print(self.data)
 
     def get_value(self, date, field):
+        """
+        Given a date, get the value, the value at the date BEFORE the selected date is always returned
+        :param date: a date given as a pandas Timestamp
+        :param field: a field
+        :return: value of the field at the given date
+        """
 
-        return self.data.loc[self.data.index == date, field]
+        try:
+            return self.data.iloc[self.data.index.get_loc(date, method='ffill')][field]
+        except KeyError:
+            return None
+
+    def is_active_project(self, date):
+        """
+        Check if the project was active at a given date
+        :param date: a date given as a pandas Timestamp
+        :return: true if the project is active, false otherwise
+        """
+        if date<self.data.index[0]:
+            return False
+        else:
+            return date - self.data.index[-1] < self.inactive_delay
+
+
+    #TODO: warning messages
+
 
 
--- parser/ProjectData_Tests.py
+++ parser/ProjectData_Tests.py
@@ -23,5 +23,22 @@ class ProjectDataTests(unittest.TestCase):
     def test_computed_missing_field(self):
         self.assertEqual(self.project.get_value(pd.Timestamp('20180304 0:0:0'), 'bodies').item(), 2.125)
 
+    def test_computed_nonexistent_field(self):
+        self.assertEqual(self.project.get_value(pd.Timestamp('20180304 0:0:0'), 'none'), None)
+
+    def test_activity(self):
+        self.assertFalse(self.project.is_active_project(pd.Timestamp('20180404 0:0:0')+pd.Timedelta(3, unit='M')))
+        self.assertTrue(self.project.is_active_project(pd.Timestamp('20180404 0:0:0') + pd.Timedelta(3, unit='M')-pd.Timedelta(1, unit='m')))
+        self.assertTrue(self.project.is_active_project(pd.Timestamp('20180304 0:0:0')))
+        self.assertTrue(self.project.is_active_project(pd.Timestamp('20180104 0:0:0')))
+        self.assertFalse(self.project.is_active_project(pd.Timestamp('20180103 0:0:0')))
+
+    def test_cpi_warning(self):
+        pass
+
+    def test_budget_warning(self):
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
