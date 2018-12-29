04c903dd24b657bde905efc81c52da2fd9680cc0
nicolas <nulysse4@gmail.com>
2018-12-29 17:37:20 -0500

    Progress in tests

--- parser/adder.py
+++ parser/adder.py
@@ -17,7 +17,6 @@ def is_active_project(data, inactive_delay, date=pd.Timestamp.now()):
         return date - data.index[-1] < inactive_delay
 
 
-
 def add_project_data(total_data, project_data, project_idx, column_to_average, inactive_delay=pd.Timedelta(3, unit='M')):
 
     index = 0
@@ -27,8 +26,8 @@ def add_project_data(total_data, project_data, project_idx, column_to_average, i
 
         if is_active_project(project_data, inactive_delay, date):
 
-
             if date in total_data.index:  # adding invoice number with current invoice data
+
                 for name, value in invoice.iteritems():
                     if name in column_to_average:
                         total_data.loc[date][name] = (total_data.loc[date][name]*project_idx + value) / (project_idx + 1)
@@ -36,14 +35,23 @@ def add_project_data(total_data, project_data, project_idx, column_to_average, i
                         total_data.loc[date][name] += value
             else:  # inserting a new date
 
-                total_data.loc[date] = invoice
+
+                if not total_data.empty and date > total_data.index[-1] and project_idx > 0:
+                    last_row = total_data.iloc[-1]
+
+                    for name, value in invoice.iteritems():
+                        if name in column_to_average:
+                            total_data.loc[date][name] = (last_row[name] * project_idx + value) / (project_idx + 1)
+                        else:
+                            total_data.loc[date][name] = value + last_row[name]
+
+                else:
+                    total_data.loc[date] = invoice
+
+
+                #
                 #current_index = total_data.index.get_loc(date)
                 #total_data.loc[date] = total_data.loc[date] + total_data.iloc[current_index -1]  # todo, normalize CPI
-
-                if index == len(project_data) - 1:  # if there are customer financial data AFTER the last date of the project
-                    print('----------- last row----------')
-                    # go through all the remaning row in the ppoject...and add!
-                    print(total_data.loc[date+ pd.Timedelta(1, unit='d'): date + inactive_delay])
 
             # todo, consider case where one project starts before the other
             # todo: Consider case where project is missing invoices (week(s))
@@ -51,6 +59,12 @@ def add_project_data(total_data, project_data, project_idx, column_to_average, i
 
         prev_date = date
         index += 1
+
+    if index == len(project_data) - 1:  # if there are customer financial data AFTER the last date of the project
+        print("last date")
+        #print('----------- last row----------')
+        # go through all the remaning row in the ppoject...and add!
+        #print(total_data.loc[date + pd.Timedelta(1, unit='d'): date + inactive_delay])
 
     total_data.sort_index(inplace=True)
     total_data.sort_index(axis=1, inplace=True)
--- parser/adder_test.py
+++ parser/adder_test.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
+
 class AdderTests(unittest.TestCase):
     """
     Test in normal conditions
@@ -29,10 +30,7 @@ class AdderTests(unittest.TestCase):
         expected_result = pd.DataFrame(index=invoice_weeks, data={'X': [50, 60, 70],
                                                                   'Y': [10030, 20040, 30050],
                                                                   'CPI': [55, 20, 65]}, dtype=float)
-
-
         expected_result.sort_index(inplace=True, axis=1)
-
 
         self.assertTrue(total.equals(expected_result))
 
@@ -46,16 +44,46 @@ class AdderTests(unittest.TestCase):
                                                            'CPI': [10, 20, 30]})
 
         for idx in range(5):
-            print(idx)
             adder.add_project_data(total, project, idx, column_to_average=('CPI'))
-            print(total)
 
         expected_result = pd.DataFrame(index=invoice_weeks, data={'X': [10*5, 20*5, 30*5],
                                                                   'CPI': [10, 20, 30]}, dtype=float)
 
         expected_result.sort_index(inplace=True, axis=1)
 
-        print(total)
         self.assertTrue(total.equals(expected_result))
 
+    def test_finishing_late(self):
+        total = pd.DataFrame(columns=['X', 'Y', 'CPI'], dtype=float)
 
+        date_today = datetime.now()
+        project1_invoice_weeks = pd.date_range(date_today, periods=3, freq='W')
+
+        project1 = pd.DataFrame(index=project1_invoice_weeks, data={'X': [40, 50, 60],
+                                                                    'Y': [10000, 20000, 30000],
+                                                                    'CPI': [10, 20, 30]})
+
+        project2_invoice_weeks = pd.date_range(date_today+pd.Timedelta(3, unit='W'), periods=3, freq='W')
+
+        project2 = pd.DataFrame(index=project2_invoice_weeks, data={'X': [10, 10, 10],
+                                                                    'Y': [30, 40, 50],
+                                                                    'CPI': [100, 20, 100]})
+
+        for idx, project in enumerate((project1, project2)):
+            adder.add_project_data(total, project, idx, column_to_average=('CPI'))
+
+        expected_results_weeks = project1_invoice_weeks.union(project2_invoice_weeks)
+
+        expected_result = pd.DataFrame(index=expected_results_weeks, data={ 'X': [40, 50, 60, 70, 70, 70],
+                                                                            'Y': [10000, 20000, 30000, 30000+30, 30000+40, 30000+50],
+                                                                            'CPI': [10, 20, 30,
+                                                                                    (100+30)/2, (20+30)/2, (100+30)/2]},
+                                                                            dtype=float)
+        expected_result.sort_index(inplace=True, axis=1)
+
+        print("expected")
+        print(expected_result)
+        print("calculated")
+        print(total)
+
+        self.assertTrue(total.equals(expected_result))
